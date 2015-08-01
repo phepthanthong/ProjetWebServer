3104f1a3bd8d58c11d9e1c61dd4b2f7d1b13a248
phepthanthong <phepthanthong@gmail.com>
2015-08-01 14:28:23 +0700

    synchronize repository

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),            
             new PUF\PlatformBundle\PUFPlatformBundle(),
         );
 
